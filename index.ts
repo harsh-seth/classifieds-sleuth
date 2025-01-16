86c4baca35bd5fbf87ce901e34b49da2954aa339
Harsh Seth <hseth@umass.edu>
2025-01-17 00:29:00 +0530

    feat(listings): Moved portal specific details to config

--- index.ts
+++ index.ts
@@ -1,37 +1,35 @@
 import qs from "query-string";
+import {getProperty} from 'dot-prop';
+import listingConf from './listing_config.json';
 
 // # ------------ #
-const url = "https://paypal.eightfold.ai/api/apply/v2/jobs";
-const query = {
-  domain: ["paypal.com"],
-  location: "United States",
-  query: "Engineering",
-  start: "0",
-  num: "25",
-  sort_by: "relevance",
-};
-const target = qs.stringifyUrl({ url, query });
-
 const headers = {
   "content-type": "application/json",
 };
 
 // # ------------ #
-
 const getBody = async (url, headers) => {
   const res = await fetch(url, {
     headers: headers,
     method: "GET",
     body: null,
   });
+  console.log(res.status)
   return await res.json();
 };
 
 // # ------------ #
 
-// Get one page of results
-getBody(target, headers).then((body) => console.log(body?.positions));
+// For each company in listing
+Object.keys(listingConf).forEach(company => {
+  const { url, query, positions_key } = listingConf[company];
+  const target = qs.stringifyUrl({ url, query });
+  // Get one page of results
+  getBody(target, headers).then((body) => console.log(getProperty(body, positions_key)));
 
-// TODO: Get multiple pages without api timeouts
+  // TODO: Get multiple pages without api timeouts
 
-// TODO: Get from multiple domains
+  // TODO: Persist results somewhere
+});
+
+// TODO: Compute diff b/w persisted results
