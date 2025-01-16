be1ec29be699b72e74e8672ac400aafbc8d333b0
Harsh Seth <hseth@umass.edu>
2025-01-17 01:19:09 +0530

    feat(listings): multi page search setup for each company

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import qs from "query-string";
-import {getProperty} from 'dot-prop';
-import listingConf from './listing_config.json';
+import { getProperty } from "dot-prop";
+import listingConf from "./listing_config.json";
 
 // # ------------ #
 const headers = {
@@ -14,20 +14,54 @@ const getBody = async (url, headers) => {
     method: "GET",
     body: null,
   });
-  console.log(res.status)
   return await res.json();
 };
 
-// # ------------ #
-
-// For each company in listing
-Object.keys(listingConf).forEach(company => {
-  const { url, query, positions_key } = listingConf[company];
+const getCompanyPositions = async (conf, meta) => {
+  const { url, query, positions_key } = conf;
+  const { company } = meta;
   const target = qs.stringifyUrl({ url, query });
-  // Get one page of results
-  getBody(target, headers).then((body) => console.log(getProperty(body, positions_key)));
 
-  // TODO: Get multiple pages without api timeouts
+  const positions: object[] = [];
+  let result: object[] | undefined = [];
+  let pageNum = 1;
+
+  // Get results from all search pages
+  while (true) {
+    try {
+      // Get one page of results
+      console.log(`Querying: "${company}" [page: ${pageNum}]`);
+      const body = await getBody(target, headers);
+      result = getProperty(body, positions_key);
+    } catch {
+      console.log(
+        `Failure when querying "${company}" [page ${pageNum}] - exiting early`
+      );
+      break;
+    }
+
+    // Checking if last page or invalid query
+    if (!result || result.length == 0) break;
+
+    // Accumulate results
+    positions.push(...result);
+
+    // Prepare for next page search
+    pageNum++;
+    break; // TODO: Remove this and add next page logic (needs more portal examples to build general solution)
+    // TODO:: Add timeouts to prevent ratelimiting
+  }
+  console.log(
+    `Done querying: "${company}". Found ${positions.length} positions`
+  );
+  return positions;
+};
+
+Object.keys(listingConf).forEach(async (company) => {
+  // Get results for each company in listing config
+  getCompanyPositions(listingConf[company], { company }).then((positions) =>
+    console.log(positions.length)
+  );
 
   // TODO: Persist results somewhere
 });
